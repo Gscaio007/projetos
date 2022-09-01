57674669156684d7d6017fdd022a620238fd61ed
Gscaio007 <112596848+Gscaio007@users.noreply.github.com>
2022-08-31 22:51:46 -0300

    Update and rename intrucoes.php.txt to intrucoes.php

---
+++ intrucoes.php
@@ -0,0 +1,339 @@
+<!DOCTYPE html>
+  
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>Conjuban - PE</title>
+
+  <!-- CSS  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  
+  <style>
+      
+  @font-face { font-family: Gulzar; src: url('Gulzar-Regular.ttf'); } 
+ @font-face { font-family: Gulzar; font-weight: bold; src: url('Gulzar-Regular.ttf');}
+ 
+ @media only screen and (max-width: 980px){
+        #h4conjuban { display: none; }
+    } 
+ 
+ @media only screen and (max-width: 980px){
+        #h4conjuban { display: none; }
+    }
+      
+  </style>
+  
+  
+  
+</head>
+<body>
+  <!-- <nav class="light-blue lighten-1" role="navigation"> -->
+  
+
+
+
+
+<hr style = "width: 100%;height: 8px;margin-left: auto;margin-right: auto;background-color:blanchedalmond;border: 0 none;margin-top: 0px;margin-bottom:0px";></hr>
+
+<nav class=" grey lighten-5" role="navigation">
+      
+    <div class="nav-wrapper container">
+        
+        
+
+<ul id=slide-out class=sidenav>
+    <li><div class=user-view>
+      <div class=background>
+   
+      </div>
+   
+     
+
+
+    </div></li>
+    <li><a href=login.php><i class=material-icons>power_settings_new
+    </i>Fazer Login</a></li>
+    <li><a href=instrucoes.php>Cadastro <i class=material-icons>input</i></a></li> 
+    <li><div class=divider></div></li>
+    <li><a class=waves-effect href=index.php><i class=material-icons>info_outline</i>Início</a></li> 
+    
+    
+  </ul>
+  <a href=# data-target=slide-out class="sidenav-trigger show-on-large"><i class=material-icons style=color:#203F7A;>menu</i></a>
+        
+        
+        
+        
+        
+        
+        <a id="logo-container" href="index.php" class="brand-logo"><img src="images/jubanpng2.png" style="width:90px; height:65px; padding-left:15px">  </a>
+    
+    
+    <a href="#" class="brand-logo right"> <img src="images/cbn.png" style="width: min(50vw, 120px); height:min(50vw, 35px); padding-left:15px"></a>
+    
+     <div  class="nav-wrapper container" ><a id="logo-container" href="#" class="brand-logo center"> <h4 id="h4conjuban" style="font-family: Gulzar; color:#203F7A;">CONJUBAN - PE </h4></a>
+     
+     <ul id="nav-mobile" class="right hide-on-med-and-down">
+         
+         
+     </ul>
+    
+  
+    
+    
+    
+      <ul class"nav-mobile" class="right hide-on-med-and-down">
+       
+            
+            
+            <!--
+            <a class="black-text text-darken-1" href="#"><img src="images/cbn.png" style="width:90px; height:65px; padding-left:15px"></a></li>
+            -->
+            
+               
+  
+            
+      </ul>
+      
+     
+
+
+
+
+
+
+
+
+
+
+     
+      
+    </div>
+   
+    <div class="nav-content">
+      <ul class="tabs tabs-transparent ">
+        
+        <!--
+        
+        <li class="tab"><a class="active" href="index.php"> Início</a></li>
+        <li class="tab"><a href="Bíblia.php"> Bíblia </a></li>
+     
+        <li class="tab"><a href="IBMBJEstudos.php"> Estudos da semana</a></li>
+        <li class="tab"><a href="IBMBJLive.php"> Live(Em breve)</a></li>
+        <li class="tab"><a href="IBMBJMissoes.php"> Missões</a></li>
+        <li class="tab"><a href="IBMBJNoticias.php"> Notícias</a></li>
+         -->
+
+
+      </ul>
+      
+      
+      
+    </div>
+ 
+   
+   
+  </nav>
+  
+    <ul class="sidenav" id="mobile-demo">
+    <li><a href="administrativo.html">Meu Perfil</a></li>
+    <li><a href="login.html">Entrar</a></li>
+    <li><a href="cadastrar.html">Cadastrar</a></li>
+  </ul>
+   
+  
+  
+  
+
+  
+ 
+  
+  <div class="section no-pad-bot" id="index-banner">
+    <div class="container">
+      <br><br>
+      <h1 class="header center orange-text">INSTRUÇÕES</h1>
+      <div class="row center">
+        <h5 class="header col s12 light">Siga as instruções abaixo para realizar sua inscrição. (escolha apenas uma opção palestra e uma opção de Workshop, pois as palestras serão realizadas simultaneamente durante a tarde. O mesmo vale para os Workshops.)
+        </h5>
+      </div>
+      <div class="row center">
+        <a href="jubanpeinsc.php" id="download-button" class="btn-large waves-effect waves-light orange">Iniciar</a>
+      </div>
+      <br><br>
+
+    </div>
+  </div>
+
+
+  <div class="container">
+    <div class="section">
+
+       
+      <div class="row">
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center light-blue-text"><i class="material-icons">group</i></h2>
+            <h5 class="center">PALESTRA</h5>
+
+            <p class="light">Selecione um tema de palestra.</p>
+          </div>
+        </div>
+
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center light-blue-text"><i class="material-icons">settings</i></h2>
+            <h5 class="center">WORKSHOP</h5>
+
+            <p class="light">   Selecione um tema de Workshop. </p>
+          </div>
+        </div>
+
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center light-blue-text"><i class="material-icons">account_circle</i></h2>
+            <h5 class="center">FORMULÁRIO</h5>
+
+            <p class="light">Preencha o formulário de cadastro para finalizar sua inscrição.</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <br><br>
+  </div>
+
+
+  <footer class="page-footer orange">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">JUBAN - Pernambuco</h5>
+          <p class="grey-text text-lighten-4">JUBAN é a sigla da Juventude Batista Nacional. É um departamento da CBN para a juventude. A JUBAN-PERNAMBUCO é a seção deste departamento do Estado de Pernambuco.</p>
+
+
+        </div>
+        <div class="col l3 s12">
+          
+          <ul>
+            <li><a class="hidden" class="white-text" href="#!"><br></a></li>
+            <li><a class="hidden" class="white-text" href="#!"><br></a></li>
+            <li><a class="hidden" class="white-text" href="#!"><br></a></li>
+            <li><a class="hidden" class="white-text" href="#!"><br></a></li>
+          </ul>
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Informações</h5>
+          <ul>
+            <li><a class="white-text" href="#!"><i class="material-icons">location_on</i> Rua Emílio Monteiro Fonseca Nº 1368 Ibura, Recife <br>     </a></li>
+            <li><a class="white-text" class="white-text" href="#!"><br> WhatsApp: 81 99522-7599 <a href="https://api.whatsapp.com/send?l=pt&amp;phone=55081995227599"><img 
+src="https://i.imgur.com/ryESuZ5.png" style="height:64px; position:fixed; bottom: 25px; right: 
+25px; z-index:99999;" data-selector="img"></a> </a></li>
+            <li><a class="hidden" class="white-text" href="#!"><br></a></li>
+            <li><a class="hidden" class="white-text" href="#!"><br></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      Made by <a class="orange-text text-lighten-3" href="#">Multimídia IBMBJ</a>
+      </div>
+    </div>
+  </footer>
+
+<!--
+  <footer class="page-footer orange">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">Company Bio</h5>
+          <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+
+
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Settings</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Connect</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      Made by <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+      </div>
+    </div>
+  </footer>
+  
+
+-->
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+  
+  
+  
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+
+
+<script src="https://code.iconify.design/2/2.0.3/iconify.min.js"></script>
+  
+  
+   <script>
+
+
+
+  // Or with jQuery
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true,
+    
+
+  });
+        
+  autoplay();
+function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 6000);
+}
+
+
+
+
+  </script>
+  
+  
+    <script>
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+
+</script>
+
+  </body>
+</html>
